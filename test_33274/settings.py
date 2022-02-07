508630b0cc60bc422d6897cfae3a5fda6effb339
itech@cyberservices.com <itech@cyberservices.com>
2022-02-07 22:39:32 +0100

    sendg

--- test_33274/settings.py
+++ test_33274/settings.py
@@ -206,7 +206,7 @@ EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
