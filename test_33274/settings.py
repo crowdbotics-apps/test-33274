7d73a7c0c27eda5c325737da2975c336ae86dfbd
itech@cyberservices.com <itech@cyberservices.com>
2022-02-08 02:42:55 +0100

    ssg

--- test_33274/settings.py
+++ test_33274/settings.py
@@ -197,15 +197,15 @@ LOGOUT_REDIRECT_URL = '/'
 
 YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY')
 
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY') 
+
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'apikey'
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST_USER = 'apikey' # value of your apikey
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+FROM_EMAIL = os.getenv("FROM_EMAIL")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
-FROM_EMAIL = os.getenv('FROM_EMAIL')
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
